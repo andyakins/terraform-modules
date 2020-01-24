ae8f882b98b95406234b229c1e8f63e95c9afd02
Andy Akins <andy_akins@mac.com>
2020-01-24 15:26:05 -0500

    Basic work...

--- terraform-site/main.tf
+++ terraform-site/main.tf
@@ -1,4 +1,4 @@
 resource "aws_codecommit_repository" "SiteRepo" {
   repository_name = "${var.site_name}-site"
-  description     = "Repository for code/resources for ${var.site_name} site."
+  description     = "Repository for code/resources for the ${var.site_name} site."
 }

e37485b19f8d3640c4ef6d6bcb493f0399279c26
Andy Akins <andy_akins@mac.com>
2020-01-24 16:23:37 -0500

    Bug fix

--- terraform-site/main.tf
+++ terraform-site/main.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "CallerID" {}
+
 resource "aws_codecommit_repository" "SiteRepo" {
   repository_name = "${var.site_name}-site"
   description     = "Repository for code/resources for the ${var.site_name} site."
@@ -6,7 +8,10 @@ resource "aws_codecommit_repository" "SiteRepo" {
 resource "aws_s3_bucket" "PublicBucket" {
   bucket = "${var.site_name}.${var.domain_extension}"
   acl    = "public-read"
-  policy = templatefile("${path.module}/public-bucket-policy.tmpl", { bucket_name = "${var.site_name}.${var.domain_extension}" })
+  policy = templatefile("${path.module}/public-bucket-policy.tmpl", {
+    bucket_name = "${var.site_name}.${var.domain_extension}",
+    user        = data.aws_caller_identity.CallerID.arn
+  })
   region = var.region
   website {
     index_document = "index.html"

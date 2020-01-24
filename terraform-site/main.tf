f17dbcc5e6c5ebe7eed8384be4c4a17cf698fb5b
Andy Akins <andy_akins@mac.com>
2020-01-24 15:41:02 -0500

    Basic work...

--- terraform-site/main.tf
+++ terraform-site/main.tf
@@ -2,3 +2,13 @@ resource "aws_codecommit_repository" "SiteRepo" {
   repository_name = "${var.site_name}-site"
   description     = "Repository for code/resources for the ${var.site_name} site."
 }
+
+resource "aws_s3_bucket" "PublicBucket" {
+  bucket = "${var.name}.com"
+  acl    = "public-read"
+  policy = templatefile("${path.module}/public-bucket-policy.tmpl", { site_name = var.site_name)
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
